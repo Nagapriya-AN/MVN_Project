7150aa58ce800ff27f01109e8983b62cd1fbeec4
Admin <Admin@DESKTOP-SPN26A2>
2025-01-22 11:05:45 +0530

    fetching title

--- mvnproject/src/test/java/mvnproject_google/Google.java
+++ mvnproject/src/test/java/mvnproject_google/Google.java
@@ -9,6 +9,7 @@ public class Google {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		System.out.println("Hello world");
+		System.out.println(driver.getTitle());
 		driver.quit();
 	}
 
